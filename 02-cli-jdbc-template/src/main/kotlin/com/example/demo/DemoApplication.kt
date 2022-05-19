0edae797e0429ba548ead1984c907b6f833ec130
Msksgm <msksgm3124@gmail.com>
2022-05-20 07:52:58 +0900

    add: customer テーブルの鈴木または佐藤を取得する処理を追加

--- 02-cli-jdbc-template/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ 02-cli-jdbc-template/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -7,15 +7,17 @@ import org.springframework.boot.runApplication
 import org.springframework.jdbc.core.JdbcTemplate
 
 @SpringBootApplication
-class DemoApplication(val jdbcTemplate: JdbcTemplate): ApplicationRunner {
-	override fun run(args: ApplicationArguments?) {
-		InsertCommandImpl(jdbcTemplate).perform("John", "Smith")
-		println("--------")
-		SelectAllQueryImpl(jdbcTemplate).perform().forEach { println(it) }
-		println("--------")
-	}
+class DemoApplication(val jdbcTemplate: JdbcTemplate) : ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
+        InsertCommandImpl(jdbcTemplate).perform("John", "Smith")
+        println("--------")
+        SelectAllQueryImpl(jdbcTemplate).perform().forEach { println(it) }
+        println("--------")
+        SelectSatoOrSuzukiQueryImpl(jdbcTemplate).perform().forEach { println(it) }
+        println("--------")
+    }
 }
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+    runApplication<DemoApplication>(*args)
 }
--- 02-cli-jdbc-template/src/main/kotlin/com/example/demo/SelectSatoOrSuzukiQuery.kt
+++ 02-cli-jdbc-template/src/main/kotlin/com/example/demo/SelectSatoOrSuzukiQuery.kt
@@ -3,18 +3,22 @@ package com.example.demo
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Component
 
-interface SelectAllQuery {
+interface SelectSatoOrSuzukiQuery {
     fun perform(): List<Customer>
 }
 
 @Component
-class SelectAllQueryImpl(val jdbcTemplate: JdbcTemplate) : SelectAllQuery {
+class SelectSatoOrSuzukiQueryImpl(val jdbcTemplate: JdbcTemplate) : SelectSatoOrSuzukiQuery {
     val sql = """
         SELECT id, first_name, last_name
-        FROM customer;
+        FROM customer
+        WHERE first_name IN (?, ?);
     """.trimIndent()
     val mapper = CustomerRowMapper()
+    val firstName1 = "鈴木"
+    val firstName2 = "佐藤"
+
     override fun perform(): List<Customer> {
-        return jdbcTemplate.queryForStream(sql, mapper).toList()
+        return jdbcTemplate.queryForStream(sql, mapper, firstName1, firstName2).toList()
     }
 }
