67fcff54b807e8b7a3c326481b382e6685a6fbc0
Msksgm <msksgm3124@gmail.com>
2022-05-20 06:34:18 +0900

    add: make コマンドを追加

--- 01-cli-helloworld/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ 01-cli-helloworld/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,11 +1,19 @@
 package com.example.demo
 
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication : ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
+        println(getHello())
+    }
+
+    fun getHello(): String = "HelloWorld"
+}
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+    runApplication<DemoApplication>(*args)
 }
--- 01-cli-helloworld/src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ 01-cli-helloworld/src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,13 +1,22 @@
 package com.example.demo
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class DemoApplicationTests {
+    @Test
+    fun instanceHasAGetHelloMethod() {
+        val instance = DemoApplication()
+        assertNotNull(instance.getHello(), "getHello メソッドを持っている")
+    }
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun getHelloMethodReturnHelloWorldString() {
+        val expected = "HelloWorld"
+        val actual = DemoApplication().getHello()
+        assertEquals(expected, actual, "getHelloメソッドは\"HelloWorld\"を返す")
+    }
 }
