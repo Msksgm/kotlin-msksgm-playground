71b2512bba72e11cdc04fb6c6f444579b6c31ed7
Msksgm <msksgm3124@gmail.com>
2022-05-20 07:21:11 +0900

    add: customer table に John, Smith を挿入する処理を追加

--- 02-cli-jdbc-template/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ 02-cli-jdbc-template/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,10 +1,18 @@
 package com.example.demo
 
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.jdbc.core.JdbcTemplate
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication(val jdbcTemplate: JdbcTemplate): ApplicationRunner {
+	override fun run(args: ApplicationArguments?) {
+		InsertCommandImpl(jdbcTemplate).perform("John", "Smith")
+		println("--------")
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
